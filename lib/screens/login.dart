a1f5aef8f8d63cad6a1cbbc43c698687e198cc4f
bceliktug <bulutcan.celiktug@gmail.com>
2021-10-29 00:31:30 +0300

    dizayn
    deneme

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -9,8 +9,85 @@ class Login extends StatelessWidget {
       body: Container(
         decoration: BoxDecoration(
             image: DecorationImage(
-                image: AssetImage('images/login.png'), fit: BoxFit.scaleDown)),
+                image: AssetImage("images/login.jpg"), fit: BoxFit.fill)),
+        child: Container(
+          child: Center(
+              child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(200),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromRGBO(143, 148, 251, .2),
+                      blurRadius: 20.0,
+                      offset: Offset(0, 10),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(color: Colors.grey.shade100),
+                        ),
+                      ),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "Email",
+                          hintStyle: TextStyle(
+                            color: Colors.grey[400],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Password",
+                            hintStyle: TextStyle(color: Colors.grey[400])),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )),
+        ),
       ),
     );
   }
 }
+
+/* Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("images/login.jpg"), fit: BoxFit.fill)),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(200)),
+                    labelText: 'User Name',
+                    hintText: 'User name or E-mail',
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),*/
