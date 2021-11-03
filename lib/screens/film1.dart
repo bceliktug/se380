3a4550251bf7d89960a8c4cd2fd3342312c875ee
bceliktug <bulutcan.celiktug@gmail.com>
2021-11-03 12:28:53 +0300

    m
    m

--- lib/screens/film1.dart
+++ lib/screens/film1.dart
@@ -8,6 +8,7 @@ class Film1 extends StatelessWidget {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
+          color: Colors.white,
           image: DecorationImage(
               image: AssetImage("images/venom2.jpg"), fit: BoxFit.fitHeight),
         ),
@@ -19,6 +20,13 @@ class Film1 extends StatelessWidget {
               width: 200,
               margin: EdgeInsets.fromLTRB(50, 200, 50, 0),
               decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 2,
+                    ),
+                  ],
                   color: Colors.white.withOpacity(0.9),
                   borderRadius: BorderRadius.circular(200)),
               child: Column(
@@ -29,7 +37,7 @@ class Film1 extends StatelessWidget {
                         padding: EdgeInsets.fromLTRB(30, 40, 30, 50),
                       ),
                       onPressed: () {},
-                      child: Text("hi man")),   /*Circle Image eklenicek */
+                      child: Text("hi man")), /*Circle Image eklenicek */
                 ],
               ),
             ),
