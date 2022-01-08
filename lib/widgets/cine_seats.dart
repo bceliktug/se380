7d74b3363b0316906e0caed3d4b85ddee87b5154
Bulutcan Çeliktuğ <bulutcan.celiktug@gmail.com>
2022-01-08 22:35:21 +0300

    seat numbers and rows

--- lib/widgets/cine_seats.dart
+++ lib/widgets/cine_seats.dart
@@ -12,10 +12,10 @@ class CineSeats {
   });
 
   static List<CineSeats> listSeats = [
-    CineSeats(freeSeats: [1, 2, 3, 5, 6, 8], seatNum: 6, seatRow: 'A'),
-    CineSeats(freeSeats: [1, 3, 6, 8], seatNum: 6, seatRow: 'A'),
-    CineSeats(freeSeats: [1, 2, 3, 6, 8], seatNum: 6, seatRow: 'A'),
-    CineSeats(freeSeats: [1, 2, 8], seatNum: 6, seatRow: 'A'),
-    CineSeats(freeSeats: [1, 2, 5, 6, 7, 8], seatNum: 6, seatRow: 'A'),
+    CineSeats(freeSeats: [1, 2, 3, 5, 6, 8], seatNum: 8, seatRow: 'A'),
+    CineSeats(freeSeats: [1, 3, 6, 8], seatNum: 8, seatRow: 'B'),
+    CineSeats(freeSeats: [1, 2, 3, 6, 8], seatNum: 8, seatRow: 'C'),
+    CineSeats(freeSeats: [1, 2, 8], seatNum: 8, seatRow: 'D'),
+    CineSeats(freeSeats: [1, 2, 5, 6, 7, 8], seatNum: 8, seatRow: 'E'),
   ];
 }
